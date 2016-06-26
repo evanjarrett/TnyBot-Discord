8af2efbcee24d066afadef8849e6017b1026c3c2
Evan Jarrett <ejfirestar00@gmail.com>
2016-06-26 03:04:53 -0700

    fix up chat cog

--- cogs/chat.py
+++ cogs/chat.py
@@ -1,20 +1,15 @@
-import asyncio
-
 from chatterbot import ChatBot
 from chatterbot.training.trainers import ListTrainer
 
-from exceptions.expections import SameUserException
 
-
-class Chat():
+class Chat:
     _sentence = []
     _chatbot = None
 
     def __init__(self, bot):
         self.bot = bot
 
-    @asyncio.coroutine
-    def on_ready(self):
+    async def on_ready(self):
         print("listening in another class " + __name__)
 
         self._chatbot = ChatBot("Tny",
@@ -22,26 +17,13 @@ class Chat():
             database="chatterbot-database"
         )
 
-        self._chatbot.set_trainer(ListTrainer)
-        self._chatbot.train([
-            "What music do you like?",
-            "kpop",
-            "favorite group?",
-            "I.O.I",
-            "who is your bias?",
-            "Chungha",
-        ])
-
         # Get a response to an input statement
 
-    @asyncio.coroutine
-    def on_message(self, message):
-        try:
-            super().on_message(message)
-        except SameUserException:
+    async def on_message(self, message):
+        if message.author == self.bot.user:
             return
 
         self._sentence = message.content.split(" ")
 
         response = self._chatbot.get_response(message.content)
-        yield from self.bot.send_message(message.channel, response)
+        await self.bot.send_message(message.channel, response)
