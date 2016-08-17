766ea68b279dd95ebe95eb24a3ed0e3ed444e4f8
Evan Jarrett <ejfirestar00@gmail.com>
2016-08-16 21:53:55 -0700

    attempt to use the content-type if the url doesn't have an extension

--- cogs/attachments.py
+++ cogs/attachments.py
@@ -2,6 +2,7 @@ import asyncio
 import configparser
 import os
 import imghdr
+import shutil
 
 from urllib import request as urllib_request
 from urllib.error import HTTPError
@@ -84,7 +85,12 @@ class Attachments:
         pic_name = parts[-1]
         if "unknown" in pic_name:
             pic_name = parts[-2] + pic_name
-        file_path = Attachments.add_extension(dirs + pic_name)
+        file_path = dirs + pic_name
+        if not Attachments.has_extension(file_path):
+            res = urllib_request.urlopen(url)
+            img_ext = res.info().get_content_subtype()
+            pic_name += ("." + img_ext)
+            file_path += ("." + img_ext)
 
         if not os.path.exists(dirs):
             print("making new directory: " + dirs)
@@ -103,11 +109,11 @@ class Attachments:
             print("already have that image: " + pic_name)
 
         ext = imghdr.what(file_path)
-        if ext is not None and not file_path.endswith(ext):
-            if not (ext == "jpeg" and file_path.endswith("jpg")):
-                print("!! This image extension should be changed to {}".format(ext))
+        if ext is not None and not file_path.lower().endswith(ext):
+            if not (ext == "jpeg" and file_path.lower().endswith("jpg")):
+                print("!! Making a copy with {} extension".format(ext))
                 file, curr_ext = os.path.splitext(file_path)
-                os.rename(file_path, "{}.{}".format(file, ext))
+                shutil.copy(file_path, "{}.{}".format(file, ext))
 
     def get_directory(self, channel):
         server_dir = channel.server.name
@@ -119,16 +125,9 @@ class Attachments:
         return dirs
 
     @staticmethod
-    def add_extension(file_path):
-        if not Attachments.has_extension(file_path):
-            # Default to jpeg for now, this will fix sites like tistory
-            file_path += ".jpg"
-        return file_path
-
-    @staticmethod
     def has_extension(file_path):
         for e in ["jpg", "jpeg", "png", "gif"]:
-            # I really only care about these, anything else probably wont work
+            # I really only care about these, anything else probably wont download correctly anyways
             if file_path.lower().endswith("." + e):
                 return True
         return False
