7187085e025431a1141e521bde1fd5df1091046b
Evan Jarrett <ejfirestar00@gmail.com>
2016-10-21 19:13:43 -0700

    test without async

---
+++ tests/database/test_doesthiswork.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+import unittest
+
+
+class TestConfigDB(unittest.TestCase):
+    def setUp(self):
+        # Make our own connection to test things.
+        # noinspection PyArgumentList
+        self.connection = sqlite3.connect("file::memory:?cache=shared", uri=True)
+        self.cursor = self.connection.cursor()
+        self.connection.commit()
+
+    def tearDown(self):
+        self.connection.close()
+
+    def test_this_database(self):
+        q = '''CREATE TABLE IF NOT EXISTS reminders
+            (user_id    TEXT    NOT NULL,
+            message     TEXT    NOT NULL,
+            remind_date INT     NOT NULL)'''
+        self.cursor.execute(q)
+        self.connection.commit()
+        self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='reminders'")
+        result = self.cursor.fetchone()
+        self.assertTupleEqual(result, ("reminders",))
