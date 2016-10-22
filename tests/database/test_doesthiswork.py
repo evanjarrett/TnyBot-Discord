e64855f70621c20b3ff5e239cc77e5556a45bc94
Evan Jarrett <ejfirestar00@gmail.com>
2016-10-21 19:35:51 -0700

    do 2 connections work?

--- tests/database/test_doesthiswork.py
+++ tests/database/test_doesthiswork.py
@@ -5,7 +5,7 @@ import unittest
 from tests import AsyncTestCase
 
 
-class TestConfigDB(AsyncTestCase):
+class TestConfigDB(unittest.TestCase):
     def setUp(self):
         # Make our own connection to test things.
         # noinspection PyArgumentList
@@ -13,16 +13,22 @@ class TestConfigDB(AsyncTestCase):
         self.cursor = self.connection.cursor()
         self.connection.commit()
 
+        # noinspection PyArgumentList
+        self.connection2 = sqlite3.connect("file::memory:?cache=shared", uri=True)
+        self.cursor2 = self.connection2.cursor()
+        self.connection2.commit()
+
     def tearDown(self):
         self.connection.close()
 
-    async def test_this_database(self):
+    def test_this_database(self):
         q = '''CREATE TABLE IF NOT EXISTS reminders
             (user_id    TEXT    NOT NULL,
             message     TEXT    NOT NULL,
             remind_date INT     NOT NULL)'''
-        self.cursor.execute(q)
-        self.connection.commit()
+        self.cursor2.execute(q)
+        self.connection2.commit()
+
         self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='reminders'")
         result = self.cursor.fetchone()
         self.assertTupleEqual(result, ("reminders",))
