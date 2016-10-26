275e7f8a551743dba83d1dcd95d100c968d17c74
Evan Jarrett <ejfirestar00@gmail.com>
2016-10-25 23:41:28 -0700

    rearrange code

--- heroku.py
+++ heroku.py
@@ -1,15 +1,15 @@
 import os
 
 from src.basicbot import BasicBot
-from src.cogs import Commands, Notifications, Grep, Roles
-from src.cogs import Greetings
+from src.cogs import Commands, Notifications, Grep, Roles, Greetings, CustomCommands
 
 heroku = BasicBot(command_prefix="!", description="""Bot built for discord's oauth bot api""")
 
 heroku.add_cog(Commands(heroku))
 heroku.add_cog(Grep(heroku))
+heroku.add_cog(CustomCommands(heroku, db_url=os.environ["DATABASE_URL"]))
+heroku.add_cog(Greetings(heroku))
 heroku.add_cog(Notifications(heroku, db_url=os.environ["DATABASE_URL"]))
-heroku.add_cog(Greetings(heroku, db_url=os.environ["DATABASE_URL"]))
 heroku.add_cog(Roles(heroku, db_url=os.environ["DATABASE_URL"]))
 
 heroku.run(os.environ["TOKEN"])
