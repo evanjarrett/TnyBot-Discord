3cc25bbf2f0ebe0cb763cb5f24565e028f969780
Evan Jarrett <ejfirestar00@gmail.com>
2016-10-25 22:43:33 -0700

    add to heroku bot

--- heroku.py
+++ heroku.py
@@ -2,12 +2,14 @@ import os
 
 from src.basicbot import BasicBot
 from src.cogs import Commands, Notifications, Grep, Roles
+from src.cogs import Greetings
 
 heroku = BasicBot(command_prefix="!", description="""Bot built for discord's oauth bot api""")
 
 heroku.add_cog(Commands(heroku))
-heroku.add_cog(Notifications(heroku, db_url=os.environ["DATABASE_URL"]))
 heroku.add_cog(Grep(heroku))
-heroku.add_cog(Roles(heroku, os.environ["DATABASE_URL"]))
+heroku.add_cog(Notifications(heroku, db_url=os.environ["DATABASE_URL"]))
+heroku.add_cog(Greetings(heroku, db_url=os.environ["DATABASE_URL"]))
+heroku.add_cog(Roles(heroku, db_url=os.environ["DATABASE_URL"]))
 
 heroku.run(os.environ["TOKEN"])
