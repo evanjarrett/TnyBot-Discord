c91047bb160dcdbd9ff53c70b50f31a8d2ad0d03
Evan Jarrett <ejfirestar00@gmail.com>
2017-01-08 17:47:40 -0800

    try caching

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -3,7 +3,7 @@ import os
 from .config import Config
 
 
-def full_path(relative_path: str) -> str:
+def full_path(relative_path: str) -> str:  # pragma: no cover
     this_file_path = os.path.dirname(os.path.abspath(__file__))
     if not relative_path.startswith("/"):
         relative_path = "/" + relative_path
